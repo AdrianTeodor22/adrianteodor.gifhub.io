e3972fa7740f6fe5b7d9d1bc36949d3766c63196
AdrianTeodor22 <124488976+AdrianTeodor22@users.noreply.github.com>
2023-02-09 19:04:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-# Joc de ghicire a personalitatilor stiintifice
+const words = [
+  "Albert Einstein a revolutionat teoria cuantica.",
+  "Isaac Newton a descoperit legea gravitationala universală.",
+  "Marie Curie a fost prima femeie care a primit Premiul Nobel.",
+  "Galileo Galilei a fost un pionier în astronomie.",
+  "Stephen Hawking a fost un fizician teoretician de renume mondial."
+];
 
-personalitati_stiintifice = [    {"nume": "Isaac Newton", "perioada": "secolul 17", "domeniu": "fizica, matematica"},    {"nume": "Albert Einstein", "perioada": "secolul 20", "domeniu": "fizica"},    {"nume": "Stephen Hawking", "perioada": "secolul 20", "domeniu": "astrofizica, cosmologie"},    {"nume": "Galileo Galilei", "perioada": "secolul 17", "domeniu": "astronomie, fizica"},    {"nume": "Marie Curie", "perioada": "secolul 19 si 20", "domeniu": "chimie, fizica, medicina"},    {"nume": "Nikola Tesla", "perioada": "secolul 19 si 20", "domeniu": "electrotehnica, fizica"}]
+const word = words[Math.floor(Math.random() * words.length)];
+const missingWord = word.split(" ")[3];
+const splitWord = word.split(" ");
+splitWord[3] = "__________";
+const question = splitWord.join(" ");
 
-import random
+document.getElementById("question").innerHTML = question;
+document.getElementById("answer").innerHTML = "Raspuns: " + missingWord;
 
-def ghiceste_personalitatea(personalitati_stiintifice):
-    personalitate = random.choice(personalitati_stiintifice)
-    nume = personalitate["nume"]
-    perioada = personalitate["perioada"]
-    domeniu = personalitate["domeniu"]
-    
-    print("Informatii despre o personalitate stiintifica:")
-    print("Perioada:", perioada)
-    print("Domeniu de activitate:", domeniu)
-    
-    incercare = input("Cine credeti ca este aceasta personalitate stiintifica? ")
-    if incercare.lower() == nume.lower():
-        print("Felicitari! Ati ghicit!")
-    else:
-        print("Din pacate, raspunsul este incorect. Personalitatea stiintifica era", nume)
-        
-ghiceste_personalitatea(personalitati_stiintifice)
+
